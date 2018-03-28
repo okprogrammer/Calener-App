22424ab6f5af55ebda6617ef1ca8e01df6889502
okprogrammmer <omk7912@gmail.com>
2018-03-28 23:43:48 +0530

    creating models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
