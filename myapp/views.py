efa882b7347a19a8df167fa1df13f54a2c5018ba
okprogrammmer <omk7912@gmail.com>
2018-03-28 23:45:36 +0530

    creating views

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Entry
 
 # Create your views here.
+def index(request):
+    entries = Entry.objects.all()
+    return render(request,'myapp/index.html',{"entries":entries})
