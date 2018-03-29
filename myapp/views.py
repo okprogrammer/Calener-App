18f8a9f38b5eeb96ca188f29fe6c20fea0fa9dc9
okprogrammmer <omk7912@gmail.com>
2018-03-29 16:30:53 +0530

    modified and deleted other page

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Entry
+from .forms import EntryForm
 
 # Create your views here.
 def index(request):
     entries = Entry.objects.all()
     return render(request,'myapp/index.html',{"entries":entries})
+
+def details(request,pk):
+    entry = Entry.objects.get(id=pk)
+    return render(request,'myapp/details.html',{"entry":entry})
+
+def add(request):
+
+    if request.method =='POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            date = form.cleaned_data['date']
+            description = form.cleaned_data['description']
+
+            Entry.objects.create(
+                name = name,
+                date = date,
+                description = description,
+            ).save()
+            return HttpResponseRedirect('/')
+        
+    else:
+        form = EntryForm()
+
+    return render(request,'myapp/form.html',{'form':form})
+
+   
