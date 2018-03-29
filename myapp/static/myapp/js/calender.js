556c297f1a8b34d90b8798896cb50fc0e8b3713d
okprogrammmer <omk7912@gmail.com>
2018-03-29 17:16:45 +0530

    static files for js

---
+++ myapp/static/myapp/js/calender.js
@@ -0,0 +1,12 @@
+function deleteCalenderEntry(entry){
+    var $entry = $(entry)
+    $(entry).parent().remove()
+    var id = $entry.data('id')
+    $.ajax({
+        url: 'entry/delete/'+id,
+        method: 'DELETE',
+        beforeSend:function(xhr){
+            xhr.setRequestHeader('X-CSRFToken',csrf_token)
+        },
+    })
+}
