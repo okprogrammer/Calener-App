184819ba98cd368602531373ec8c7a1bd99b05bf
okprogrammmer <omk7912@gmail.com>
2018-03-29 22:51:19 +0530

    creating an another field for author

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Entry(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
     date = models.DateTimeField()
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
