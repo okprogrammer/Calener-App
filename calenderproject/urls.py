fe737dcc56ecf37d28cd7ed7a2b1f390df97c33a
okprogrammmer <omk7912@gmail.com>
2018-03-29 16:21:43 +0530

    creating add and detail urls

--- calenderproject/urls.py
+++ calenderproject/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('',views.index),
+    path('',views.index,name='index'),
+    path('entry/<int:pk>',views.details,name='details'),
+    path('entry/add',views.add,name='add'),
     path('admin/', admin.site.urls),
 ]
